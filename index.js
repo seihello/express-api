309729c9becc16fbf3ef642b7bc250748b80b8a4
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-09-08 09:24:35 -0700

    feat: Create a root endpoint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import server from "./server";
+import server from "./server.js";
 
 const HOST = "localhost";
 const PORT = "3009"
--- server.js
+++ server.js
@@ -2,4 +2,10 @@ import express from "express";
 
 const server = express();
 
+server.get("/", (req, res) => {
+  console.log("/ is working");
+  res.send("/ is working");
+});
+
+
 export default server;
