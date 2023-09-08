9b1704280f4ebe5d07403072ade1d1cf56d7503e
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-09-08 09:48:56 -0700

    feat: Craete a router for todo id

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,14 @@
 import express from "express";
+//import data from "../data/todos.json" assert { type : "json"};
+
 const todosRouter = express.Router();
 
 todosRouter.get("/", (req, res) => {
   res.send("todos router is working");
+});
+
+todosRouter.get("/:id", (req, res) => {
+  console.log(req.params.id);
 });
 
 export default todosRouter;
